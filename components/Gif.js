5e7ec1ff62da52991191682c8f2d60a4b760884c
Bartek <bartekma89@gmail.com>
2018-02-04 00:51:49 +0100

    added inline style to gif.js

--- components/Gif.js
+++ components/Gif.js
@@ -1,5 +1,10 @@
 var GIPHY_LOADING_URL = 'http://tf2glass.com/trade/spinner.gif';
 
+var styles = {
+	minHeight: '310px',
+	margin: '0.5em'
+}
+
 Gif = React.createClass({
 	
 	getURL: function () {
@@ -10,7 +15,7 @@ Gif = React.createClass({
 		var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
 
 		return (
-				<div id='gif'>
+				<div id='gif' style={styles}>
 					<a href={this.getURL()} alt='view this on githy' target='new'>
 						<img 
 							id='gif' 
